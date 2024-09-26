be94707bf570cd58f3dc4c699d871826ca07ef47
MarkTBSS <marktbss@gmail.com>
2024-09-27 05:18:28 +0700

    [ADD] First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install
+
+```sh
+npm install -g live-server
+```
+
+# Run
+
+```sh
+live-server
+node script.js
+```
---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+    return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(" ");
+    return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+    console.log("👋");
+};
+
+// Check if running in a browser environment
+if (typeof document !== 'undefined') {
+    document.body.addEventListener("click", high5);
+    ["Jonas", "Martha", "Adam"].forEach(high5);
+} else {
+    console.log("This code is running in a Node.js environment.");
+}
